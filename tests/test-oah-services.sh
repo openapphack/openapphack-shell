3fcf51cea671ffa62f653baa038107ccd1ae3b3f
Vinod Panicker <vinod.panicker@wipro.com>
2016-08-02 01:21:19 +0530

    updated oah structure

---
+++ tests/test-oah-services.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Global variables
+OAH_INSTALLER_SERVICE="https://openapphack.github.io/oah-installer/"
+#OAH meta data service for validated OAH environments
+#OAH_ENVS_INFO_SERVICE=https://openapphack.github.io/oah-installer/envinfo/candidates.txt
+OAH_ENVS_INFO_SERVICE="https://openapphack.github.io/oah-installer/envinfo/"
+#OAH_BROADCAST_SERVICE=https://openapphack.github.io/oah-installer/broadcast/broadcast.txt
+OAH_BROADCAST_SERVICE="https://openapphack.github.io/oah-installer/broadcast/"
+#OAH_VERSION=0.0.1a1
+OAH_VERSION="0.0.1a1"
+OAH_DIR="$HOME/.oah"
